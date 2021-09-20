f7711014cc82a59d77a27d674eefbf0fb8002740
Xander Vertegaal <73882506+XanderVertegaal@users.noreply.github.com>
2021-09-20 14:01:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy");
+let name = 'Xander';
+console.log(name);
+let num = 4;
+console.log(num + num); // yields 8
+num = '4';
+console.log(num + num); // yields 44
+console.log(12 + 8);
+console.log(12 - 8);
+console.log(12 * 8);
+console.log(12 / 8);
+console.log(12 % 8);
+let age = 28;
+console.log(typeof(age));
+age = '28';
